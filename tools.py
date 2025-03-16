a8a5667b124cdfbc04a21997dfaf3db304e4b20e
Sayandeep <raysayandeep@gmail.com>
2025-03-16 23:47:53 +0530

    Added new files

---
+++ tools.py
@@ -0,0 +1,31 @@
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
+
+search = DuckDuckGoSearchRun()
+search_tool = Tool(
+    name="search",
+    func=search.run,
+    description="Search the Web with Duckduckgo for information"
+)
+
+
+def math_tool(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+
+custom_math_tool = Tool(
+    name="simple_math",
+    func=math_tool,
+    description="Perform basic math operations"
+)
